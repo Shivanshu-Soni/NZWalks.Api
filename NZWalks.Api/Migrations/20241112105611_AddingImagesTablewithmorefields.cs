f9d684a9ceeb2276b47422e8604e9cbddb79da02
Shivanshu <shivanshuss13@gmail.com>
2024-11-12 16:29:24 +0530

    chnages in Nzdbcontext to add missing columns

---
+++ NZWalks.Api/Migrations/20241112105611_AddingImagesTablewithmorefields.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingImagesTablewithmorefields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Images",
+                newName: "FileSizeInBytes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Images");
+
+            migrationBuilder.RenameColumn(
+                name: "FileSizeInBytes",
+                table: "Images",
+                newName: "MyProperty");
+        }
+    }
+}
--- NZWalks.Api/obj/Debug/net8.0/NZWalks.Api.AssemblyInfo.cs
+++ NZWalks.Api/obj/Debug/net8.0/NZWalks.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910cdcd0d7859907cbe3d5d2228705728f902486")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d1724cbf3a5abbd397494cd4f214364cdd5d5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
