b75ec646f62c1a02e90e68faa40cc27d1ab0cd9e
Shivanshu <shivanshuss13@gmail.com>
2024-11-24 22:30:34 +0530

    get data

--- NZWalks.Api/obj/Debug/net8.0/NZWalks.Api.AssemblyInfo.cs
+++ NZWalks.Api/obj/Debug/net8.0/NZWalks.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1169796f5c94b6d2060eb6f2502adf42f5b6236b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78edcf990108a5856e65b2cf138e7e6ac73cd755")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NZWalks.UI/obj/Debug/net8.0/NZWalks.UI.AssemblyInfo.cs
+++ NZWalks.UI/obj/Debug/net8.0/NZWalks.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4256fb23ed9981fb4a0b7170a64e5efaa7af3dc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78edcf990108a5856e65b2cf138e7e6ac73cd755")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
